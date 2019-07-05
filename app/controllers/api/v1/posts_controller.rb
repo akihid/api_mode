beaa80a3b3ebf2ceeb7b2d1f4fa885882e58f900
sato akihide <aki@sato.local>
2019-07-06 03:16:36 +0900

    Add: コントローラーにupfate処理記載

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -26,6 +26,15 @@ module Api
         render json: { status: 'SUCCESS', message: 'deleted the post', data: post }
       end
 
+      def update
+        post = Post.find(params[:id])
+        if post.update(post_params)
+          render json: { status: 'SUCCESS', message: 'updated the post', data: post}
+        else
+          render json: { status: 'ERROR', message: 'post not saved', data: post.errors}
+        end
+      end
+
       private
 
       def post_params
