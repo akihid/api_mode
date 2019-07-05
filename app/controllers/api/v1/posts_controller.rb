adf2d332a02c0437f0693a3f59bb3a7797e5e915
sato akihide <aki@sato.local>
2019-07-06 03:30:11 +0900

    Fix: controllerの記述ミス修正

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,9 @@
 module Api
   module V1
-    class PostsContoroller < ApplicatioonContoroller
+    class PostsController < ApplicationController
       def index
         posts = Post.order(created_at: :desc)
-        render json: { statusL 'SUCCESS', message: 'loaded posts', data: posts}
+        render json: { status: 'SUCCESS', message: 'loaded posts', data: posts}
       end
 
       def show
